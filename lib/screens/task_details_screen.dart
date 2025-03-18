5deda5a9ab156829028c16ae1f595e0bc761f343
antonio venancio <antoniovenancio10@gmail.com>
2025-03-18 10:28:07 -0300

    ajsutes na criaca/edicao task screen

--- lib/screens/task_details_screen.dart
+++ lib/screens/task_details_screen.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/task_provider.dart';
-import '../models/task.dart';
+import 'package:taskone/models/task.dart';
+import 'package:taskone/providers/task_provider.dart';
+import 'package:taskone/screens/tasks/task_form_screen.dart';
 
 class TaskDetailsScreen extends StatelessWidget {
   final String taskId;
 
-   TaskDetailsScreen({Key? key, required this.taskId}) : super(key: key);
+  TaskDetailsScreen({Key? key, required this.taskId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context); // Obtendo o tema atual
+    final theme = Theme.of(context);
 
     return Scaffold(
-      backgroundColor:
-          theme.colorScheme.background, // Usando o tema para o fundo
+      backgroundColor: theme.colorScheme.background,
       appBar: AppBar(
-        backgroundColor:
-            theme.colorScheme.surface, // Usando o tema para o AppBar
-        elevation: 0, // Remove a sombra do AppBar
+        backgroundColor: theme.colorScheme.surface,
+        elevation: 0,
         title: Text(
           'Task Details',
           style: TextStyle(
@@ -59,9 +58,7 @@ class TaskDetailsScreen extends StatelessWidget {
           }
 
           final task = snapshot.data!;
-          final double progress = _calculateProgress(
-            task,
-          ); // Calcula o progresso com base nas subtasks
+          final double progress = _calculateProgress(task);
 
           return SingleChildScrollView(
             padding: const EdgeInsets.all(16.0),
@@ -133,7 +130,6 @@ class TaskDetailsScreen extends StatelessWidget {
                               size: 20,
                             ),
                             const SizedBox(width: 8),
-                            // Exibindo os avatares dos contribuidores
                             ...task.subtasks
                                 .expand((subtask) => subtask.contributors)
                                 .map((contributor) {
@@ -176,6 +172,23 @@ class TaskDetailsScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 24),
 
+                // Prioridade da tarefa
+                Text(
+                  "Priority",
+                  style: theme.textTheme.headlineSmall?.copyWith(
+                    color: theme.colorScheme.onBackground,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  task.priority,
+                  style: theme.textTheme.bodyMedium?.copyWith(
+                    color: theme.colorScheme.onSurfaceVariant,
+                  ),
+                ),
+                const SizedBox(height: 24),
+
                 // Categoria da tarefa
                 DropdownButtonFormField<String>(
                   value: task.category,
@@ -189,7 +202,6 @@ class TaskDetailsScreen extends StatelessWidget {
                         );
                       }).toList(),
                   onChanged: (value) {
-                    // Atualizar a categoria da tarefa
                     task.category = value;
                     Provider.of<TaskProvider>(
                       context,
@@ -219,7 +231,7 @@ class TaskDetailsScreen extends StatelessWidget {
                 }).toList(),
                 const SizedBox(height: 16),
 
-                // Botão para adicionar subtask
+                // Botão para editar a tarefa
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
@@ -230,9 +242,17 @@ class TaskDetailsScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(12),
                       ),
                     ),
-                    onPressed: () => _addSubtask(context, task),
+                    onPressed: () {
+                      // Navegar para a TaskFormScreen no modo de edição
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TaskFormScreen(task: task),
+                        ),
+                      );
+                    },
                     child: Text(
-                      "Add Subtask",
+                      "Edit Task",
                       style: TextStyle(
                         color: theme.colorScheme.onPrimary,
                         fontSize: 16,
@@ -252,9 +272,7 @@ class TaskDetailsScreen extends StatelessWidget {
   // Função para calcular o progresso com base nas subtasks
   double _calculateProgress(Task task) {
     if (task.subtasks.isEmpty) {
-      return task.isCompleted
-          ? 1.0
-          : 0.0; // Progresso baseado na tarefa principal
+      return task.isCompleted ? 1.0 : 0.0;
     } else {
       final completedSubtasks =
           task.subtasks.where((subtask) => subtask.isCompleted).length;
@@ -307,10 +325,7 @@ class TaskDetailsScreen extends StatelessWidget {
           value: subtask.isCompleted,
           onChanged: (value) {
             subtask.isCompleted = value ?? false;
-            _updateTaskCompletion(
-              task,
-              context,
-            ); // Atualiza o status da tarefa principal
+            _updateTaskCompletion(task, context);
           },
         ),
         title: Text(
@@ -322,7 +337,6 @@ class TaskDetailsScreen extends StatelessWidget {
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            // Exibindo os avatares dos contribuidores
             ...subtask.contributors.map((contributor) {
               return Padding(
                 padding: const EdgeInsets.only(left: 4.0),
@@ -341,116 +355,8 @@ class TaskDetailsScreen extends StatelessWidget {
   // Função para atualizar o status da tarefa principal
   void _updateTaskCompletion(Task task, BuildContext context) {
     if (task.subtasks.isNotEmpty) {
-      // Marca a tarefa como concluída se todas as subtasks estiverem concluídas
       task.isCompleted = task.subtasks.every((subtask) => subtask.isCompleted);
     }
     Provider.of<TaskProvider>(context, listen: false).updateTask(task, context);
   }
-
-  // Função para adicionar uma nova subtask
-  void _addSubtask(BuildContext context, Task task) {
-    final theme = Theme.of(context); // Obtendo o tema atual
-    final TextEditingController _subtaskController = TextEditingController();
-    final List<Map<String, String>> _selectedUsers = [];
-
-    showDialog(
-      context: context,
-      builder:
-          (context) => AlertDialog(
-            title: Text(
-              'Add Subtask',
-              style: TextStyle(color: theme.colorScheme.onSurface),
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  controller: _subtaskController,
-                  decoration: InputDecoration(
-                    hintText: 'Subtask title',
-                    hintStyle: TextStyle(
-                      color: theme.colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                // Aqui você pode adicionar um seletor de usuários (pode ser um dropdown ou uma lista de checkboxes)
-                // Exemplo simplificado:
-                Text(
-                  'Select users:',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ),
-                ..._dummyUsers.map((user) {
-                  return CheckboxListTile(
-                    title: Text(user['name'] ?? ''),
-                    value: _selectedUsers.contains(user),
-                    onChanged: (value) {
-                      if (value == true) {
-                        _selectedUsers.add(user);
-                      } else {
-                        _selectedUsers.remove(user);
-                      }
-                    },
-                  );
-                }).toList(),
-              ],
-            ),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text(
-                  'Cancel',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (_subtaskController.text.isNotEmpty) {
-                    final newSubtask = Subtask(
-                      id: DateTime.now().toString(),
-                      title: _subtaskController.text,
-                      contributors: _selectedUsers,
-                    );
-                    task.subtasks.add(newSubtask);
-                    _updateTaskCompletion(
-                      task,
-                      context,
-                    ); // Atualiza o status da tarefa principal
-                    Navigator.pop(context);
-                  }
-                },
-                child: Text(
-                  'Add',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                ),
-              ),
-            ],
-          ),
-    );
-  }
-
-  // Lista de usuários fictícia (substitua por uma lista real de usuários)
-  final List<Map<String, String>> _dummyUsers = [
-    {
-      'userId': '1',
-      'name': 'User 1',
-      'avatarUrl': 'https://via.placeholder.com/150',
-    },
-    {
-      'userId': '2',
-      'name': 'User 2',
-      'avatarUrl': 'https://via.placeholder.com/150',
-    },
-    {
-      'userId': '3',
-      'name': 'User 3',
-      'avatarUrl': 'https://via.placeholder.com/150',
-    },
-  ];
 }
